68dbd10f2e7dfa5527e4d2c0dc08fcd1a43d4e60
Nuno Costa <n.costa@efuture.nl>
2012-03-20 17:21:18 +0100

    added documentation for ftest framework

--- tests/functional/fTest/TestCase.php
+++ tests/functional/fTest/TestCase.php
@@ -1,30 +1,46 @@
 <?php
 namespace fTest;
 
+/**
+ * Base test case
+ * @author nuno costa <nuno@francodacosta.com>
+ *
+ */
 abstract class TestCase
 {
     private $result = null;
     private $title = '';
 
-    public function __construct(Result $result = null)
-    {
-        if (!is_null($result)) {
-            $this->setResult($result);
-        }
-    }
-
+    /**
+     * The test code, this code will be used in the generated documentation
+     */
     abstract public function test();
 
+    /**
+     * returns true if the test passed
+     *
+     * should be overriden in your test class
+     * @return boolean
+     */
     public function checkResults()
     {
         return true;
     }
 
+    /**
+     * Used to render the results
+     *
+     * usefull for generating the documentation
+     */
     public function renderResults()
     {
 
     }
 
+    /**
+     * gets the code for this test
+     * @return String
+     */
     public function getTestCode()
     {
         $reflector = new \ReflectionClass($this);
@@ -41,6 +57,10 @@ abstract class TestCase
         return join( '', array_slice( file( $method->getFileName() ), $start_offset, $end_offset ) );
     }
 
+    /**
+     * executes the test
+     * @throws \Exception
+     */
     public function execute()
     {
         try {
@@ -58,21 +78,31 @@ abstract class TestCase
         }
     }
 
+    /**
+     * gets the test result object
+     */
     public function getResult()
     {
         return $this->result;
     }
 
-    public function setResult(Result $result)
+    protected function setResult(Result $result)
     {
         $this->result = $result;
     }
 
+    /**
+     * gets the test title
+     */
     public function getTitle()
     {
         return $this->title;
     }
 
+    /**
+     * sets the test title
+     * @param String $title
+     */
     public function setTitle($title)
     {
         $this->title = $title;
