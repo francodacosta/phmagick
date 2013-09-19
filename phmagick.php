353f375b741f2469207f31fe6af2d4e61a46abe8
Dustin Thomson <dthomson@51systems.com>
2013-09-18 20:21:13 -0600

    phmagick 0.4.1 upstream import

--- phmagick.php
+++ phmagick.php
@@ -1,246 +1,340 @@
 <?php
+/*
+    +--------------------------------------------------------------------------------------------+
+    |   DISCLAIMER - LEGAL NOTICE -                                                              |
+    +--------------------------------------------------------------------------------------------+
+    |                                                                                            |
+    |  This program is free for non comercial use, see the license terms available at            |
+    |  http://www.francodacosta.com/licencing/ for more information                              |
+    |                                                                                            |
+    |  This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; |
+    |  without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. |
+    |                                                                                            |
+    |  USE IT AT YOUR OWN RISK                                                                   |
+    |                                                                                            |
+    |                                                                                            |
+    +--------------------------------------------------------------------------------------------+
 
-$bp = realpath(dirname(__FILE__));
-$ds = DIRECTORY_SEPARATOR;
-require_once  $bp . $ds . 'core' . $ds . 'log.php';
-require_once  $bp . $ds . 'core' . $ds . 'cli.php';
-require_once  $bp . $ds . 'core' . $ds . 'cmd.php';
-require_once  $bp . $ds . 'core' . $ds . 'file.php';
-require_once  $bp . $ds . 'core' . $ds . 'core.php';
-require_once  $bp . $ds . 'core' . $ds . 'plugin_manager.php';
-require_once  $bp . $ds . 'core' . $ds . 'exceptions.php';
-
-
+*/
 /**
+ * phMagick - Image manipulation with Image Magick
  *
- * One point entry to PhMagick
- * Encapsluates PhMagick core classes into an easy to use wrapper
- *
- * @author nuno
+ * @version    0.4.1
+ * @author     Nuno Costa - sven@francodacosta.com
+ * @copyright  Copyright (c) 2007
+ * @license    LGPL
+ * @link       http://www.francodacosta.com/phmagick
+ * @since      2008-03-13
  */
-class PhMagick{
-    private $_core          = NULL;
-    private $_fileHandler   = NULL;
-    private $_pluginManager = NULL;
-    private $_debug         = FALSE;
-    private $_cli           = NULL;
-    private $_logger        = NULL
-    ;
-    function __construct($source ='', $dest='')
-    {
-        //TODO: move creation of this objects outside PhMagick and inject them as dependency, maybe using a settings class
-        $this->_fileHandler = new PhMagickFile();
-        $this->_core = new PhMagickCore( $this->_fileHandler );
-        $f =$this->_fileHandler;
-        $p = $f->set(dirname(__FILE__))->add('plugins')->get();
-        $this->setPluginManager( new PhMagickPluginManager($p, $f) );
+class phmagick{
+    private $availableMethods = array();
+    private $loadedPlugins = array();
 
-        $this->_logger = new PhMagickLogger();
-        $this->_cli = new PhMagickCli($this->_logger);
+    private $escapeChars = null ;
 
-        $this->source($source);
-        $this->destination($dest);
+    private $history = array();
+    private $originalFile = '';
+    private $source = '';
+    private $destination = '';
+    private $imageMagickPath = '';
+    private $imageQuality = 80 ;
 
-        $this->init();
+    public $debug = false;
+    private $log = array();
 
-    }
+    function __construct($sourceFile='', $destinationFile=''){
+        $this->originalFile = $sourceFile;
 
-    private function init()
-    {
-        $p = $this->getPluginManager();
-        $p->loadAll();
+        $this->source = $sourceFile ;
+        $this->destination = $destinationFile;
 
-    }
-
-    function setLogger(PhMagickLogger $l)
-    {
-        $this->_logger = $l;
-        return $this;
-    }
-
-    function getLogger()
-    {
-        return $this->_logger;
-    }
-
-    function debug($value = TRUE)
-    {
-        $this->_debug = $value;
-        return $this;
-    }
-
-    function debugMode()
-    {
-        return $this->_debug ;
-    }
-
-    function setPluginManager(PhMagickPluginManager $v)
-    {
-        $this->_pluginManager = $v;
-        return $this;
-    }
-
-    function getPluginManager()
-    {
-        return $this->_pluginManager;
-    }
-
-    /**
-     * Sets the path to image being manipulated
-     * @param string $path
-     */
-    function setSourceFile($path)
-    {
-        $this->_core->setSourceFile($path);
-        return $this;
-    }
-
-    /**
-     * Gets the path to image being manipulated
-     */
-    function getSourceFile()
-    {
-        return $this->_core->getSourceFile();
-    }
-
-    /**
-     * Alias to getSourceFile and setSourceFile
-     *
-     * @param string $path if empty string calls getSourceFile, if not it will call setSourceFile
-     */
-    function source($path = '')
-    {
-        if ('' == $path)
-        {
-            return $this->getSourceFile();
+        if(is_null($this->escapeChars) ){
+            $this->escapeChars = !( strtolower ( substr( php_uname('s'), 0, 3))  == "win" ) ;
         }
 
-        $this->setSourceFile($path);
+        $this->loadPlugins();
+    }
+
+
+    public function getLog(){
+        return $this->log;
+    }
+    public function getBinary($binName){
+        return $this->getImageMagickPath()  . $binName ;
+    }
+
+    //-----------------
+     function setSource ($path){
+        $this->source = str_replace(' ','\ ',$path) ;
+        return $this ;
+    }
+
+     function getSource (){
+        return $this->source ;
+    }
+
+    //-----------------
+     function setDestination ($path){
+        $path = str_replace(' ','\ ',$path) ;
+        $this->destination = $path ;
         return $this;
     }
 
-	/**
-     * Sets the path to image being manipulated
-     * @param string $path
-     */
-    function setDestinationFile($path)
-    {
-        $this->_core->setDestinationFile($path);
+     function getDestination (){
+        if( ($this->destination == '')){
+            $source = $this->getSource() ;
+            $ext = end (explode('.', $source)) ;
+            $this->destinationFile = dirname($source) . '/' . md5(microtime()) . '.' . $ext;
+        }
+        return $this->destination ;
+    }
+
+    //-----------------
+
+    function setImageMagickPath ($path){
+        if($path != '')
+            if ( strpos($path, '/') < strlen($path))
+                $path .= '/';
+        $this->imageMagickPath = str_replace(' ','\ ',$path) ;
+    }
+
+     function getImageMagickPath (){
+        return $this->imageMagickPath;
+    }
+    //-----------------
+     function setImageQuality($value){
+        $this->imageQuality = intval($value);
         return $this;
     }
 
-    /**
-     * Gets the path to image being manipulated
-     */
-    function getDestinationFile()
-    {
-        return $this->_core->getDestinationFile();
+     function getImageQuality(){
+        return $this->imageQuality;
     }
 
-    /**
-     * Alias to getDestinationFile and setDestinationFile
-     *
-     * @param string $path if empty string calls getDestinationFile, if not it will call setDestinationFile
-     */
-    function destination($path = '')
-    {
-        if ('' == $path)
-        {
-            return $this->getDestinationFile();
+    //-----------------
+
+    function getHistory( $type = Null ){
+        switch ($type){
+
+            case phMagickHistory::returnCsv:
+                return explode(',', array_unique($this->history));
+                break;
+
+            default:
+            case phMagickHistory::returnArray :
+                return array_unique($this->history) ;
+                break;
+
+        }
+    }
+
+    public function setHistory($path){
+        $this->history[] = $path ;
+        return $this;
+    }
+
+    public function clearHistory(){
+        unset ($this->history);
+        $this->history = array();
+    }
+
+
+    public function requirePlugin($name, $version=null){
+
+        if(key_exists($name, $this->loadedPlugins)) {
+            if(! is_null($version)) {
+               if( property_exists($this->loadedPlugins[$name], 'version') ){
+                    if($this->loadedPlugins[$name]->version > $version)
+                       return true;
+
+                    if($this->debug) throw new phMagickException ('Plugin "'.$name.'" version ='.$this->loadedPlugins[$name]->version . ' required >= ' . $version);
+               }
+            }
+            return true ;
         }
 
-        $this->setDestinationFile($path);
-        return $this;
+        if($this->debug) throw new phMagickException ('Plugin "'.$name.'" not found!');
+        return false;
     }
 
-	/**
-     * Sets the path to ImageMagick's bin folder
-     * @param string $path
-     */
-    function setBinaryBasePath($path)
-    {
-        $this->_core->setBinaryBasePath($path);
-        return $this;
-    }
+    //-----------------
 
-    /**
-     * Gets the path to ImageMagick's bin folder
-     */
-    function getBinaryBasePath()
-    {
-        return $this->_core->getBinaryBasePath();
-    }
-
-    /**
-     * Alias to getBinaryBasePath and setBinaryBasePath
-     *
-     * @param string $path if empty string calls getBinaryBasePath, if not it will call setBinaryBasePath
-     */
-    function binary($path = '')
-    {
-        if ('' == $path)
-        {
-            return $this->getBinaryBasePath();
+    private function loadPlugins(){
+        $base = dirname(__FILE__) . '/plugins';
+        $plugins = glob($base . '/*.php');
+        foreach($plugins as $plugin){
+            include_once $plugin ;
+            $name = basename($plugin, '.php');
+            $className = 'phMagick_'.$name ;
+            $obj = new $className();
+            $this->loadedPlugins[$name] = $obj ;
+            foreach (get_class_methods($obj) as $method )
+                 $this->availableMethods[$method] = $name ;
         }
-
-        $this->setBinaryBasePath($path);
-        return $this;
     }
 
-    function __call($method, $args){
-        $plugin = $this->getPluginManager();
 
-        if( ! $plugin->methodExists($method) )
-        {
-            throw new PhMagickException_NotFound('Undefined Method : ' . $method );
-        }
+    public function execute($cmd){
 
-        $className = $plugin->getClassForMethod($method);
-        array_unshift($args, $this);
-        $ret = call_user_func_array(array($className, $method), $args);
-
-        if($ret === false)
-           throw new PhMagickException_ExecutionError('Error executing ' . $classname . '->' . $method . '(' . implode(', ', $args). ')');
-
-        return $ret ;
-    }
-
-    function execute(phMagickCmd $cmdCls)
-    {
         $ret = null ;
         $out = array();
-        $log = $this->getLogger();
-        $cmd = $cmdCls->get();
 
-        if($this->_core->isWindows()) {
+        if($this->escapeChars) {
             $cmd= str_replace    ('(','\(',$cmd);
             $cmd= str_replace    (')','\)',$cmd);
         }
-
         exec( $cmd .' 2>&1', $out, $ret);
-        $log->append(
-            array(
-                'cmd' => $cmd
-                ,'return' => $ret
-                ,'output' => $out
-            )
-        );
 
         if($ret != 0)
-        {
-            $msg = 'Error #' . $ret . ' while executing "' . $cmd . '"';
+            if($this->debug) trigger_error (new phMagickException ('Error executing "'. $cmd.'" <br>return code: '. $ret .' <br>command output :"'. implode("<br>", $out).'"' ), E_USER_NOTICE );
 
-            if ($this->debugMode())
-            {
-                $msg .= "\n Debug Log: \n" . $log->toString();
-            }
-
-            throw new PhMagickException_CliError ($msg);
-        }
-
-
-
+        $this->log[] = array(
+            'cmd' => $cmd
+            ,'return' => $ret
+            ,'output' => $out
+        );
 
         return $ret ;
     }
+
+    public function __call($method, $args){
+        if(! key_exists($method, $this->availableMethods))
+           throw new Exception ('Call to undefined method : ' . $method);
+
+           array_unshift($args, $this);
+           $ret = call_user_func_array(array($this->loadedPlugins[$this->availableMethods[$method]], $method), $args);
+
+           if($ret === false)
+              throw new Exception ('Error executing method "' . $method ."'");
+
+           return $ret ;
+    }
 }
+
+class phMagickHistory{
+    const returnArray = 0 ;
+    const returnCsv   = 1 ;
+
+    private function __construct(){}
+}
+
+
+class phMagickException extends Exception {
+
+    function __construct($message, $code=1){
+        //parent::__construct('', $code);
+        $this->message($message);
+    }
+    function message($message){
+        echo '<br><b>phMagick</b>: ' . $message ;
+    }
+}
+
+
+class phMagickGravity{
+    const None      = 'None' ;
+    const Center    = 'Center' ;
+    const East      = 'East' ;
+    const Forget    = 'Forget' ;
+    const NorthEast = 'NorthEast' ;
+    const North     = 'North' ;
+    const NorthWest = 'NorthWest' ;
+    const SouthEast = 'SouthEast' ;
+    const South     = 'South' ;
+    const SouthWest = 'SouthWest' ;
+    const West      = 'West' ;
+
+    private function __construct(){}
+}
+
+
+class phMagickTextObjectDefaults{
+    public static $fontSize ='12';
+    public static $font = false;
+
+    public static $color = '#000';
+    public static $background = false;
+
+    public static $gravity = phMagickGravity::Center; //ignored in fromString()
+    public $Text = '';
+
+    private function __construct(){}
+}
+
+
+class phMagickTextObject {
+    protected $fontSize;
+    protected $font;
+
+    protected $color;
+    protected $background;
+
+    protected $pGravity; //ignored in fromString()
+    protected $pText = '';
+
+    public function __construct(){
+        $this->fontSize   = phMagickTextObjectDefaults::$fontSize;
+        $this->font       = phMagickTextObjectDefaults::$font;
+        $this->color      = phMagickTextObjectDefaults::$color ;
+        $this->background = phMagickTextObjectDefaults::$background;
+        $this->pGravity   = phMagickTextObjectDefaults::$gravity;
+    }
+
+    function defaultFontSize($value){
+        phMagickTextObjectDefaults::$fontSize = $value;
+    }
+
+    function defaultFont($value){
+        phMagickTextObjectDefaults::$font = $value;
+    }
+
+    function defaultColor($value){
+        phMagickTextObjectDefaults::$color = $value;
+    }
+
+    function defaultBackground($value){
+        phMagickTextObjectDefaults::$background = $value;
+    }
+
+    function defaultGravity($value){
+        phMagickTextObjectDefaults::$gravity = $value;
+    }
+
+
+
+    function fontSize($i){
+        $this->fontSize = $i ;
+        return $this;
+    }
+
+    function font($i){
+        $this->font = $i ;
+        return $this;
+    }
+
+    function color($i){
+        $this->color = $i ;
+        return $this;
+    }
+
+    function background($i){
+        $this->background = $i ;
+        return $this;
+    }
+
+    function __get($var){
+        return $this->$var ;
+    }
+
+    function gravity( $gravity){
+        $this->pGravity = $gravity;
+        return $this ;
+    }
+
+    function text( $text){
+        $this->pText = $text;
+        return $this ;
+    }
+}
+
+?>
